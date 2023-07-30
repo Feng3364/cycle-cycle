fa2f0595e645b5651c6bf376574a320318fc477b
Felix <840385400@qq.com>
2023-07-30 16:13:38 +0800

    iOS常量

--- XFlash_iOS/Example/XFlash/Public/Const/ConstVC.swift
+++ XFlash_iOS/Example/XFlash/Public/Const/ConstVC.swift
@@ -13,20 +13,45 @@ class ConstVC: BaseTableVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//
+//        NSLog(@"********************设备信息********************");
+//        NSLog(@"设备名称:%@", XFlash.deviceName);
+//        NSLog(@"系统版本:%@", XFlash.systemVersion);
+//        NSLog(@"设备方向:%ld", (long)XFlash.deviceOrientation);
+//        NSLog(@"设备是否水平:%d", XFlash.isLandscape);
         resetData(with: [
-            (title: "设备尺寸", list: [
+            (title: "屏幕尺寸", list: [
+                "屏幕坐标",
                 "屏幕宽",
                 "屏幕高",
-                "",
-                "",
-                "App名称",
-                "AppID",
-                "Build",
-                "Version",
-                "设备名",
-                "设备方向",
-                ""
+                "状态栏高度",
+                "导航栏高度",
+                "顶部导航栏高度",
+                "底部安全区域高度",
+                "tabBar高度",
+                "不带tabBar且有导航栏的页面高度",
+                "带tabBar且有导航栏的页面高度",
+            ]),
+            //
+            //        NSLog(@"****************************************");
+            //        NSLog(@":%@", XFlash.displayName);
+            //        NSLog(@"应用标识:%@", XFlash.bundleID);
+            //        NSLog(@"应用build:%@", XFlash.buildNumber);
+            //        NSLog(@"应用version:%@", XFlash.versionNumber);
+            //        NSLog(@"是否刘海屏:%d", XFlash.iPhoneX);
+            //        NSLog(@"应用单例:%@", XFlash.application);
+            //        NSLog(@"主窗口:%@", XFlash.keyWindow);
+            (title: "应用信息", list: [
+                "应用名",
+                "应用标识",
+                "屏幕高",
+                "状态栏高度",
+                "导航栏高度",
+                "顶部导航栏高度",
+                "底部安全区域高度",
+                "tabBar高度",
+                "不带tabBar且有导航栏的页面高度",
+                "带tabBar且有导航栏的页面高度",
             ]),
         ])
     }
--- XFlash_iOS/Example/XFlash/Public/Const/OCConstVC.m
+++ XFlash_iOS/Example/XFlash/Public/Const/OCConstVC.m
@@ -18,12 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"应用名:%@", XFlash.displayName);
-    NSLog(@"唯一标识:%@", XFlash.bundleID);
-    
-    NSLog(@"屏幕尺寸:%@", NSStringFromCGRect(XFlash.bounds));
+    NSLog(@"********************屏幕尺寸********************");
+    NSLog(@"屏幕坐标:%@", NSStringFromCGRect(XFlash.bounds));
     NSLog(@"屏幕宽:%f", XFlash.screenWidth);
-    NSLog(@"屏幕宽:%f", XFlash.screenHeight);
+    NSLog(@"屏幕高:%f", XFlash.screenHeight);
+    NSLog(@"状态栏高度:%f", XFlash.statusBarHeight);
+    NSLog(@"导航栏高度:%f", XFlash.navBarHeight);
+    NSLog(@"顶部导航栏高度:%f", XFlash.topHeight);
+    NSLog(@"底部安全区域高度:%f", XFlash.bottomHeight);
+    NSLog(@"tabBar高度:%f", XFlash.tabBarHeight);
+    NSLog(@"不带tabBar且有导航栏的页面高度:%f", XFlash.mainHeight);
+    NSLog(@"带tabBar且有导航栏的页面高度:%f", XFlash.mainTabBarHeight);
+    
+    NSLog(@"********************应用信息********************");
+    NSLog(@"应用名:%@", XFlash.displayName);
+    NSLog(@"应用标识:%@", XFlash.bundleID);
+    NSLog(@"应用build:%@", XFlash.buildNumber);
+    NSLog(@"应用version:%@", XFlash.versionNumber);
+    NSLog(@"是否刘海屏:%d", XFlash.iPhoneX);
+    NSLog(@"应用单例:%@", XFlash.application);
+    NSLog(@"主窗口:%@", XFlash.keyWindow);
+    
+    NSLog(@"********************设备信息********************");
+    NSLog(@"设备名称:%@", XFlash.deviceName);
+    NSLog(@"系统版本:%@", XFlash.systemVersion);
+    NSLog(@"设备方向:%ld", (long)XFlash.deviceOrientation);
+    NSLog(@"设备是否水平:%d", XFlash.isLandscape);
 }
 
 @end
